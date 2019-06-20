da54256f7af75bb032616d591dd2472ab596c80f
Agnieszka Lempaszek <agnieszka.lempaszek@student.uj.edu.pl>
2019-06-20 14:21:55 +0200

    Methods moved to FallDetector object

---
+++ FallDetector.cpp
@@ -0,0 +1,106 @@
+//
+// Created by Agnieszka Lempaszek on 2019-06-20.
+//
+
+#pragma once
+
+#include "FallDetector.h"
+
+double FallDetector::getStddev(vector<double>* vec) {
+    Scalar mean, stddev;
+    if(!vec->empty()) {
+        meanStdDev(*vec, mean, stddev);
+    }
+    return stddev[0];
+}
+
+double FallDetector::getMovementCoefficient(Mat* foreground,Mat* history) {
+    double sumForeground = sum(*foreground)[0];
+    double sumHistory = sum(*history)[0];
+    return (sumHistory / sumForeground) * 100.0;
+}
+
+void FallDetector::checkIfStaysInPlace(time_t start, bool* isChecking, bool* isFall, vector<double> xPos, vector<double> yPos) {
+    double secondsSinceStart = difftime( time(0), start);
+    double xDevValue;
+    double yDevValue;
+
+//        cout << "x: [";
+//        for(size_t i = 0 ; i < xPos.size(); i++) {
+//            cout << xPos.at(i) << "  ";
+//        }
+//        cout << "]\n";
+//
+//        cout << "y: [";
+//        for(size_t i = 0 ; i < yPos.size(); i++) {
+//            cout << yPos.at(i) << "  ";
+//        }
+//        cout << "]\n";
+
+    xDevValue = this->getStddev(&xPos);
+    yDevValue = this->getStddev(&yPos);
+
+//    cout << endl << "XDev: " << xDevValue << "\n";
+//    cout << "YDev: " << yDevValue << "\n\n";
+
+    if(xDevValue < 2 && yDevValue < 2) {
+        *isFall = true;
+    }
+
+    if (!isFall && secondsSinceStart > 2) {
+        *isChecking = false;
+    }
+}
+
+void FallDetector::analyzePosition(Mat* frame, vector<double>* thetaRatio, vector<double>* aRatio, vector<double>* bRatio, vector<double>* xPos, vector<double>* yPos, vector<Point> largestContour) {
+    Rect boundingRectangle = boundingRect(largestContour);
+    rectangle(*frame, boundingRectangle, Scalar(0, 255, 0), 2);
+
+    if(largestContour.size() > 5) {
+
+        RotatedRect e = fitEllipse(largestContour);
+        ellipse( *frame, e, Scalar(255, 0, 0), 2 );
+
+        thetaRatio->push_back(e.angle);
+
+        double a = (double)e.size.width / 2.0;
+        double b = (double)e.size.height / 2.0;
+        aRatio->push_back(a);
+        bRatio->push_back(b);
+
+        double x = e.center.x;
+        double y = e.center.y;
+        xPos->push_back(x);
+        yPos->push_back(y);
+
+        if(thetaRatio->size() > 10) {
+            thetaRatio->erase(thetaRatio->begin());
+        }
+
+        if(aRatio->size() > 10) {
+            aRatio->erase(aRatio->begin());
+        }
+        if(bRatio->size() > 10) {
+            bRatio->erase(bRatio->begin());
+        }
+        if(xPos->size() > 10) {
+            xPos->erase(xPos->begin());
+        }
+        if(yPos->size() > 10) {
+            yPos->erase(yPos->begin());
+        }
+    }
+}
+
+void FallDetector::checkMovementAfterFall(bool* toBeChecked, bool *isFall, vector<double> xPos, vector<double> yPos) {
+    double xDevValue = this->getStddev(&xPos);
+    double yDevValue = this->getStddev(&yPos);
+
+    cout << "X pos: " << xDevValue;
+    cout << ", Y pos: " << yDevValue << "\n";
+
+    if(xDevValue > 2 && yDevValue > 2) {
+        *isFall = false;
+        *toBeChecked = false;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -12,20 +12,17 @@
 #include <sys/time.h>
 #include <time.h>
 
+#include "FallDetector.cpp"
+
 #define ESC 27
 
 using namespace std;
 using namespace cv;
 
-double getStddev(vector<double>* vec);
-double getMovementCoefficient(Mat* foreground,Mat* history);
-void analyzePosition(Mat* frame, vector<double>* thetaRatio, vector<double>* aRatio, vector<double>* bRatio, vector<double>* xPos, vector<double>* yPos, vector<Point> largestContour);
-void checkIfStaysInPlace(time_t start, bool* isChecking, bool* isFall, vector<double> xPos, vector<double> yPos);
-void checkMovementAfterFall(bool* toBeChecked,  bool *isFall, vector<double> xPos, vector<double> yPos);
-
 int main() {
-    VideoCapture cap("/Users/agalempaszek/Desktop/opencv-lab/falls.avi");
-    //VideoCapture cap(0);
+    VideoCapture cap(0);
+
+    FallDetector fallDetector;
 
     Mat frame, eroded, dilated;
     Mat backgroundImg, foregroundImg;
@@ -99,36 +96,34 @@ int main() {
 
 
         if(!largestContour.empty()) {
-            analyzePosition(&frame, &thetaRatio, &aRatio, &bRatio, &xPos, &yPos, largestContour);
-
+            fallDetector.analyzePosition(&frame, &thetaRatio, &aRatio, &bRatio, &xPos, &yPos, largestContour);
         }
 
         double timestamp = (double)clock() / CLOCKS_PER_SEC;
         motempl::updateMotionHistory(fgMaskMOG2, history, timestamp, 0.5);
 
-        movementCoefficientValue = getMovementCoefficient(&fgMaskMOG2, &history);
-        thetaRatioValue = getStddev(&thetaRatio);
-        aRatioValue = getStddev(&aRatio);
-        bRatioValue = getStddev(&bRatio);
+        movementCoefficientValue = fallDetector.getMovementCoefficient(&fgMaskMOG2, &history);
+        thetaRatioValue = fallDetector.getStddev(&thetaRatio);
+        aRatioValue = fallDetector.getStddev(&aRatio);
+        bRatioValue = fallDetector.getStddev(&bRatio);
 
-        cout << "Coeff: " << movementCoefficientValue << ",  Theta: " << thetaRatioValue << ", A: " << aRatioValue << ", B: " << bRatioValue << endl;
-
+        //cout << "Coeff: " << movementCoefficientValue << ",  Theta: " << thetaRatioValue << ", A: " << aRatioValue << ", B: " << bRatioValue << endl;
 
         if(!toBeChecked) {
             if(movementCoefficientValue > 80 && thetaRatioValue > 20 && (aRatioValue / bRatioValue) > 0.9 ) {
-                cout << "Check" << endl;
+                //cout << "Check" << endl;
                 toBeChecked = true;
                 start = time(0);
             }
         }
 
         if(toBeChecked) {
-            checkIfStaysInPlace(start, &toBeChecked, &isFall, xPos, yPos);
+            fallDetector.checkIfStaysInPlace(start, &toBeChecked, &isFall, xPos, yPos);
         }
 
         if(isFall) {
-            cout << "UPADEK" << endl;
-            checkMovementAfterFall(&toBeChecked, &isFall, xPos, yPos);
+            //cout << "UPADEK" << endl;
+            fallDetector.checkMovementAfterFall(&toBeChecked, &isFall, xPos, yPos);
         }
 
 
@@ -155,105 +150,4 @@ int main() {
     destroyAllWindows();
 
     return 0;
-}
-
-
-double getStddev(vector<double>* vec) {
-    Scalar mean, stddev;
-    if(!vec->empty()) {
-        meanStdDev(*vec, mean, stddev);
-    }
-    return stddev[0];
-}
-
-
-double getMovementCoefficient(Mat* foreground,Mat* history) {
-    double sumForeground = sum(*foreground)[0];
-    double sumHistory = sum(*history)[0];
-    return (sumHistory / sumForeground) * 100.0;
-}
-
-void checkIfStaysInPlace(time_t start, bool* isChecking, bool* isFall, vector<double> xPos, vector<double> yPos) {
-    double secondsSinceStart = difftime( time(0), start);
-    double xDevValue;
-    double yDevValue;
-
-//        cout << "x: [";
-//        for(size_t i = 0 ; i < xPos.size(); i++) {
-//            cout << xPos.at(i) << "  ";
-//        }
-//        cout << "]\n";
-//
-//        cout << "y: [";
-//        for(size_t i = 0 ; i < yPos.size(); i++) {
-//            cout << yPos.at(i) << "  ";
-//        }
-//        cout << "]\n";
-
-    xDevValue = getStddev(&xPos);
-    yDevValue = getStddev(&yPos);
-
-//    cout << endl << "XDev: " << xDevValue << "\n";
-//    cout << "YDev: " << yDevValue << "\n\n";
-
-    if(xDevValue < 2 && yDevValue < 2) {
-        *isFall = true;
-    }
-
-    if (!isFall && secondsSinceStart > 2) {
-        *isChecking = false;
-    }
-}
-
-void analyzePosition(Mat* frame, vector<double>* thetaRatio, vector<double>* aRatio, vector<double>* bRatio, vector<double>* xPos, vector<double>* yPos, vector<Point> largestContour) {
-    Rect boundingRectangle = boundingRect(largestContour);
-    rectangle(*frame, boundingRectangle, Scalar(0, 255, 0), 2);
-
-    if(largestContour.size() > 5) {
-
-        RotatedRect e = fitEllipse(largestContour);
-        ellipse( *frame, e, Scalar(255, 0, 0), 2 );
-
-        thetaRatio->push_back(e.angle);
-
-        double a = (double)e.size.width / 2.0;
-        double b = (double)e.size.height / 2.0;
-        aRatio->push_back(a);
-        bRatio->push_back(b);
-
-        double x = e.center.x;
-        double y = e.center.y;
-        xPos->push_back(x);
-        yPos->push_back(y);
-
-        if(thetaRatio->size() > 10) {
-            thetaRatio->erase(thetaRatio->begin());
-        }
-
-        if(aRatio->size() > 10) {
-            aRatio->erase(aRatio->begin());
-        }
-        if(bRatio->size() > 10) {
-            bRatio->erase(bRatio->begin());
-        }
-        if(xPos->size() > 10) {
-            xPos->erase(xPos->begin());
-        }
-        if(yPos->size() > 10) {
-            yPos->erase(yPos->begin());
-        }
-    }
-}
-
-void checkMovementAfterFall(bool* toBeChecked, bool *isFall, vector<double> xPos, vector<double> yPos) {
-    double xDevValue = getStddev(&xPos);
-    double yDevValue = getStddev(&yPos);
-
-    cout << "X pos: " << xDevValue;
-    cout << ", Y pos: " << yDevValue << "\n";
-
-    if(xDevValue > 2 && yDevValue > 2) {
-        *isFall = false;
-        *toBeChecked = false;
-    }
 }
